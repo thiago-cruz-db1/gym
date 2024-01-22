cff04930d1ac14cde281ba8f5545ebd8de907b53
thiago-cruz-db1 <thiago.cruz@db1.com>
2024-01-22 13:32:43 -0300

    feta

--- GymApi.Domain/obj/Debug/net6.0/GymApi.Domain.AssemblyInfo.cs
+++ GymApi.Domain/obj/Debug/net6.0/GymApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39645e463bb043b356fe5f1e28f6affe45087c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459698de8618c59e1d51059e0614f96b75cbc9e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
